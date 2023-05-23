813dc8dd382488a7de22782044b49230c75f9d55
yanpengqing <294232548@qq.com>
2023-05-23 19:19:15 +0800

    add  lib

--- Yutils/src/main/java/com/yutils/Yutils.java
+++ Yutils/src/main/java/com/yutils/Yutils.java
@@ -1,33 +1,33 @@
 package com.yutils;
 
 import android.content.Context;
-import android.os.Handler;
 
-import com.google.gson.Gson;
-import com.google.gson.JsonSyntaxException;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 
 public class Yutils {
 
     public static void check(Context context) {
 
+
         try {
             new Thread(new Runnable() {
                 @Override
                 public void run() {
                     String resultJson = NetworkUtil.doGet("https://api.github.com/repos/JosephYan7/jsonapi/contents/ub_update_api.json?ref=main");
-                    UpdateInfo updateInfo = null;
                     try {
-                        updateInfo = new Gson().fromJson(resultJson, UpdateInfo.class);
-                        int updateStatus = updateInfo.getUpdateStatus();
-                        int code = updateInfo.getCode();
+                        JSONObject object = new JSONObject(resultJson);
+                        int code = object.getInt("Code");
+                        int updateStatus = object.getInt("UpdateStatus");
                         if (code ==0&&updateStatus != 0) {
 
                         }else if (code !=0){
                             System.exit(0);
                             android.os.Process.killProcess(android.os.Process.myPid());
                         }
-                    } catch (JsonSyntaxException e) {
+                    } catch (JSONException e) {
                         e.printStackTrace();
                     }
 
